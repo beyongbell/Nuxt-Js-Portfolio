45b47aeb88005e21a03c88f87a9dafbb2431a6d4
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-11-18 14:23:29 +0700

    Create Navbar and Detail Slug

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">{{ post.title }}</h1>
+
+      <nuxt-content :document="post" />
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const post = await $content('blog', params.slug).fetch()
+    const title = post.title
+    return { post, title }
+  },
+  data() {
+    return {
+      title: 'Blog Post',
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+    }
+  },
+  middleware: 'auth',
+}
+</script>
+
+<style></style>
